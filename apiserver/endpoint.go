c111361d32d0effcd81b2651a5148d7872ae3ae8
Turao <lenz.arthur@gmail.com>
2021-04-17 15:18:20 -0300

    Add more endpoints. What is even happening at this point?

---
+++ apiserver/endpoint.go
@@ -0,0 +1,96 @@
+package apiserver
+
+import (
+	"context"
+	"log"
+
+	"github.com/go-kit/kit/endpoint"
+)
+
+type DispatchRequest struct {
+	Name string
+	Args []string
+}
+
+type DispatchResponse struct {
+	ID string `json:"id"`
+}
+
+func makeDispatchEndpoint(service Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		log.Println("[dispatch endpoint]", "called")
+		req := request.(DispatchRequest)
+		id, err := service.Dispatch(ctx, req.Name, req.Args...)
+		if err != nil {
+			return nil, err
+		}
+
+		return DispatchResponse{ID: id}, nil
+	}
+}
+
+type StopRequest struct {
+	ID string
+}
+
+type StopResponse struct{}
+
+func makeStopEndpoint(service Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		log.Println("[stop endpoint]", "called")
+		req := request.(StopRequest)
+		err := service.Stop(ctx, req.ID)
+		return nil, err
+	}
+}
+
+type QueryInfoRequest struct {
+	ID string
+}
+type QueryInfoResponse struct {
+	ID       string `json:"id"`
+	Status   string `json:"id"`
+	ExitCode *int   `json:"exitCode,omitempty"`
+}
+
+func makeQueryInfoEndpoint(service Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		log.Println("[query info endpoint]", "called")
+		req := request.(QueryInfoRequest)
+		info, err := service.QueryInfo(ctx, req.ID)
+		if err != nil {
+			return nil, err
+		}
+
+		return QueryInfoResponse{
+			ID:       info.Id,
+			Status:   info.Status,
+			ExitCode: info.ExitCode,
+		}, nil
+	}
+}
+
+type QueryLogsRequest struct {
+	ID string
+}
+
+type QueryLogsResponse struct {
+	Output string `json:"output,omitempty"`
+	Errors string `json:"errors,omitempty"`
+}
+
+func makeQueryLogsEndpoint(service Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		log.Println("[query logs endpoint]", "called")
+		req := request.(QueryLogsRequest)
+		logs, err := service.QueryLogs(ctx, req.ID)
+		if err != nil {
+			return nil, err
+		}
+
+		return QueryLogsResponse{
+			Output: logs.Output,
+			Errors: logs.Errors,
+		}, nil
+	}
+}
