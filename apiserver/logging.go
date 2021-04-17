0211c60ab2a3243687a262bd35ab1f2fdbbbb2da
Turao <lenz.arthur@gmail.com>
2021-04-17 13:49:59 -0300

    Adhere to global convention of naming decorated stuff 'next'

--- apiserver/logging.go
+++ apiserver/logging.go
@@ -6,29 +6,29 @@ import (
 )
 
 type loggingMiddleware struct {
-	Service
+	next Service
 }
 
 func (l *loggingMiddleware) Dispatch(ctx context.Context, name string, args ...string) (string, error) {
 	log.Println("[Dispatch]", "called")
 	defer log.Println("[Dispatch]", "finished")
-	return l.Service.Dispatch(ctx, name, args...)
+	return l.next.Dispatch(ctx, name, args...)
 }
 
 func (l *loggingMiddleware) Stop(ctx context.Context, jobId string) error {
 	log.Println("[Stop]", "called")
 	defer log.Println("[Stop]", "finished")
-	return l.Service.Stop(ctx, jobId)
+	return l.next.Stop(ctx, jobId)
 }
 
 func (l *loggingMiddleware) QueryInfo(ctx context.Context, jobId string) (JobInfo, error) {
 	log.Println("[QueryInfo]", "called")
 	defer log.Println("[QueryInfo]", "finished")
-	return l.Service.QueryInfo(ctx, jobId)
+	return l.next.QueryInfo(ctx, jobId)
 }
 
 func (l *loggingMiddleware) QueryLogs(ctx context.Context, jobId string) (JobLogs, error) {
 	log.Println("[QueryLogs]", "called")
 	defer log.Println("[QueryLogs]", "finished")
-	return l.Service.QueryLogs(ctx, jobId)
+	return l.next.QueryLogs(ctx, jobId)
 }
